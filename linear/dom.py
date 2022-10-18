112f0d54bdcad2efcc3be80a732390d404145f63
Sifan Ding <dingsf6@gmail.com>
2022-10-18 18:24:07 +0800

    linear optimization of basin DoM

--- linear/dom.py
+++ linear/dom.py
@@ -21,3 +21,9 @@ def ti(x:np.ndarray, mu:float, vw:float, gap:float, cw:float) -> np.ndarray:
     bulk = np.logical_or(np.logical_and(x>bv, x<tv), np.logical_and(x>bc, x<tc))
     edge = np.logical_and(x>bv, x<tc)
     return (edge*2+bulk*2)*2
+
+def basin(x:np.ndarray, mu:float, gap:float) -> np.ndarray:
+    tv = -gap/2 +mu # top of valence band
+    bc = gap/2 +mu # bottom of conducting band
+    low = np.logical_and(x>tv, x<bc)
+    return (4-2*low)*2

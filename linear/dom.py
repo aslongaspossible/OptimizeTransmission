b136afb661162e29d2d07c4fa35d7e4d5557ad1d
Sifan Ding <dingsf6@gmail.com>
2022-10-18 17:47:12 +0800

    linear optimization

---
+++ linear/dom.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+
+def const(_:float) -> float:
+    """Constant density of modes."""
+    return 1.
+
+def ti(x:np.ndarray, mu:float, vw:float, gap:float, cw:float) -> np.ndarray:
+    """DoM of TIs.
+    
+    Args:
+        x: E/(kBT)
+        mu: Energy of the center of gap.
+        vw: Width of valence band.
+        cw: Width of conducting band.
+    """
+    tv = -gap/2 +mu # top of valence band
+    bc = gap/2 +mu # bottom of conducting band
+    bv = tv -vw # bottom of valence band
+    tc = bc +cw # top of conducting band
+    bulk = np.logical_or(np.logical_and(x>bv, x<tv), np.logical_and(x>bc, x<tc))
+    edge = np.logical_and(x>bv, x<tc)
+    return (edge*2+bulk*2)*2
---
+++ linear/equation.py
@@ -0,0 +1,116 @@
+import numpy as np
+from scipy import integrate
+from scipy.optimize import root
+from collections import deque
+
+
+def kernel(x:float, j:int, dom) -> float:
+    """Return $e^xx^jM(x)/(1+e^x)^2$.
+
+    Args:
+        x: E/(kBT)
+        dom: density of modes
+    """
+    ex = np.exp(-abs(x)) # To avoid overflow.
+    return x**j *dom(x) * ex/(1+ex)**2
+
+def I(x1, x2, j, dom) -> float:
+    """Return $I_j$.
+    
+    Args:
+        x1: start position of transport window
+        x2: end position of transport window
+        dom: density of modes
+    """
+    return integrate.quad(kernel, x1, x2, args=(j,dom))[0]
+
+
+def eq(x:list, beta:float, dom) -> list[float]:
+    """Equations"""
+    numerator = I(x[0], x[1], 2, dom) +beta
+    # 把除法都变成乘法，好处是不容易发散，且 Jacobian 不含 beta。
+    return [
+        (x[0]+x[1]) *I(x[0],x[1],1,dom) -2*numerator,
+        x[0]*x[1]*I(x[0],x[1],0,dom) -numerator
+    ]
+
+def jac(x:list[float], _, dom) -> np.ndarray:
+    """Jacobian
+    
+    Args:
+        _: Should be beta, but Jacobian is independent of beta.
+    """
+    int1 = I(x[0], x[1], 1, dom)
+    int0 = I(x[0], x[1], 0, dom)
+    sumx = x[0] + x[1]
+    prodx = x[0]*x[1]
+    k2x0 = kernel(x[0],2,dom)
+    k2x1 = kernel(x[1],2,dom)
+    return np.array([
+        [
+            int1 -sumx*kernel(x[0],1,dom) +2*k2x0,
+            int1 +sumx*kernel(x[1],1,dom) -2*k2x1
+        ],
+        [
+            x[1]*int0 -prodx*kernel(x[0],0,dom) +k2x0,
+            x[0]*int0 +prodx*kernel(x[1],0,dom) -k2x1
+        ]
+    ])
+
+
+def solve_eq(guess:list, beta:float, dom) -> list[float]:
+    """Solve for x and zT
+    
+    Args:
+        guess: A guess for result
+        dom: density of modes
+        
+    Returns:
+        x1: start position of transport window
+        x2: end position of transport window
+        zT: thermoelectric figure of merit
+        pf: power factor in the unit of $k_B^2/h$
+        seebeck: Seebeck coefficient in the unit of $k_B/e$
+    """
+    x1, x2 = root(eq, guess, args=(beta,dom), jac=jac).x
+    prodx = x1*x2
+    seebeck = 2*prodx /(x1+x2)
+    return [x1, x2,
+        4*prodx /(x1-x2)**2,
+        seebeck**2 *I(x1,x2,0,dom),
+        seebeck
+    ]
+
+
+def solve4beta(dom, beta_range=np.logspace(-3,3,num=301), guess=[1.6, 8.9]):
+    """Solve optimal transport window for certain density of modes and beta parameters.
+    
+    Args:
+        dom: Density of modes.
+        
+    Returns:
+        result_total: Stack of results. Each result contains:
+            1/beta: equation parameter
+            x1: start position of transport window
+            x2: end position of transport window
+            zT: figure of merit
+            pf: power factor in the unit of $k_B^2/h$
+            seebeck: Seebeck coefficient in the unit of $k_B/e$
+        """
+    start_index = len(beta_range)//2
+    beta = beta_range[start_index]
+    result = solve_eq(guess, beta, dom)
+    result_total = deque([[1/beta]+result])
+    guess0 = [result[0], result[1]]
+    guess = guess0
+    for beta in beta_range[start_index+1:]:
+        result = solve_eq(guess, beta, dom)
+        result_total.appendleft([1/beta]+result)
+        guess = [result[0], result[1]]
+    guess = guess0
+    for beta in beta_range[start_index-1::-1]:
+        result = solve_eq(guess, beta, dom)
+        result_total.append([1/beta]+result)
+        guess = [result[0], result[1]]
+    # print(result_total)
+    return np.vstack(result_total)
